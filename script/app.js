6401f92e880f88558b523b6cb065c9a2dd632e7a
Rodolfo Silva <100287751+Jrodolfosilva@users.noreply.github.com>
2023-12-02 22:37:41 -0300

    feat: section services & portfolio

--- script/app.js
+++ script/app.js
@@ -1,10 +1,11 @@
 const menuShow =  document.querySelector('.show')
 const buttonToggleMenu = document.querySelector('.toggle_menu_mobile')
-
+const body = document.querySelector('body')
 
 
 
 buttonToggleMenu.addEventListener('click',function(){
     buttonToggleMenu.classList.toggle('close')
     menuShow.classList.toggle('hidden');
+    body.classList.toggle('no_scroll')
 })
