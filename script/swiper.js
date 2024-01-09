f25c0d038b6a1dea7ca68cdd54328d85acb8d782
Rodolfo Silva <100287751+Jrodolfosilva@users.noreply.github.com>
2024-01-09 12:27:18 -0300

    feat: slide home swiper

--- script/swiper.js
+++ script/swiper.js
@@ -14,4 +14,22 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
   
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+       
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    }
   });
